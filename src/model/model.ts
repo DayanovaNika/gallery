4a26ffaedb2739bc0f75029e751333bcf996050a
Nika <Dayanova27@gmail.com>
2025-09-29 17:51:53 +0500

    feat: add image info

--- src/model/model.ts
+++ src/model/model.ts
@@ -12,6 +12,8 @@ export class Model {
     async query(query: string) {
         const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=4jmoKzKcB0CftDxEyOY4dvzdW2-iliOIf3Oza8ERicc&query=${query}&per_page=10&page=1`)
         const data = await response.json()
+        console.log(data);
+        
         return data
     }
 } 
--- src/view/components/main-view.ts
+++ src/view/components/main-view.ts
@@ -36,6 +36,28 @@ const fadeParams = {
     tagName: "div",
     classList: [style.mainFade],
 };
+const wrapperInfoParams = {
+    tagName: "div",
+    classList: [style.wrapperInfo],
+};
+const likesWrapperParams = {
+    tagName: "div",
+    classList: [style.likesWrapper],
+};
+const likeElementParams = {
+    tagName: "div",
+    classList: [style.likeElement],
+};
+const counterParams = {
+    tagName: "div",
+    text: "",
+    classList: [style.counter],
+};
+const photoDescParams = {
+    tagName: "p",
+    text: "",
+    classList: [style.photoDesc],
+};
 
 export class MainView {
     element
@@ -53,13 +75,23 @@ export class MainView {
     createImageList(data: DataTypes) {
         const template = new DocumentFragment()
         this.clear()
-        console.log(data);
         
         data.results.forEach((imageInfo)=> {
             const li = new Creator(liParams).getElement()
             li.style.backgroundColor = imageInfo.color;
             const image = new Creator(imageParams).getElement() as HTMLImageElement
             image.src = imageInfo.urls.regular
+            const wrapperInfo = new Creator(wrapperInfoParams).getElement() as HTMLElement
+            const likesWrapper = new Creator(likesWrapperParams).getElement() as HTMLElement
+            const likeElement = new Creator(likeElementParams).getElement() as HTMLElement
+            counterParams.text = imageInfo.likes // проверить позже
+            const counter = new Creator(counterParams).getElement() as HTMLElement
+            photoDescParams.text = imageInfo.alt_description
+            const photoDesc = new Creator(photoDescParams).getElement() as HTMLElement
+
+            wrapperInfo.append(likesWrapper, photoDesc)
+            likesWrapper.append(likeElement, counter)
+            li.append(wrapperInfo)
             li.append(image)
             template.append(li)
         })
@@ -75,15 +107,14 @@ export class MainView {
         loaderContainer.append(gif)
         fade.append(loaderContainer)
         this.element.append(fade)
-        // loaderContainer.style.display = 'none'
-        // лоадер изначально должен быть скрыт
+        
         return fade
     }
     removeLoader() {
         setTimeout(() => {
             console.log("Delayed for 1 second.");
             this.loader.classList.add(style.loaderHidden);
-        },"1000");
+        },1000);
     }
     showLoader() {
         this.loader.classList.remove(style.loaderHidden)
