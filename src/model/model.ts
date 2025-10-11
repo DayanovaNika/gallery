332d32711e9d2148fa53d2166cc7e10a6e2adc01
Nika <Dayanova27@gmail.com>
2025-10-11 11:02:38 +0500

    TODO: need fixed getData method

--- src/model/model.ts
+++ src/model/model.ts
@@ -2,18 +2,32 @@ export class Model {
     defaultValue
     constructor(){
         this.defaultValue = "puppy"
+        this.getData("1.4", undefined, "genres", "genres.name", "movie")
     }
-    async getData(event: Event) {
-        const formData = new FormData(event.target as HTMLFormElement)
-        const query = formData.get("search") as string
-        const dataFromServer = await this.query(query)
-        return dataFromServer
-    }
-    async query(query: string) {
-        const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=4jmoKzKcB0CftDxEyOY4dvzdW2-iliOIf3Oza8ERicc&query=${query}&per_page=10&page=1`)
+    // async getData(event: Event) {
+    //     const formData = new FormData(event.target as HTMLFormElement)
+    //     const query = formData.get("search") as string
+    //     const dataFromServer = await this.query(query)
+    //     return dataFromServer
+    // }
+    // придумать способ различать энд поинты 
+    async getData(versionAPIParams=1, limitItemsParams=10, selectFieldsParams="", endPointParams="", chapterParams="", queryParams="") {
+        const options = {
+            method: 'GET',
+            headers: {accept: 'application/json', 'X-API-KEY': 'S31WEXH-H65M0YV-K1VT142-0Y4QAMT'}
+        };
+        const baseURL = "https://api.kinopoisk.dev"
+        const numberOfPage = "1"
+        const versionAPI = versionAPIParams
+        const limitItems = limitItemsParams
+        const endPoint = endPointParams
+        const chapter = chapterParams
+        const query = queryParams
+        const selectFields = selectFieldsParams
+        const response = await fetch(`${baseURL}/v${versionAPI}/${chapter}?page=${numberOfPage}&limit=${limitItems}&${selectFields}=&${endPoint}=${query}`, options) 
         const data = await response.json()
+        console.log(`${baseURL}/v${versionAPI}/${chapter}?page=${numberOfPage}&limit=${limitItems}&${selectFields}=&${endPoint}=${query}`);
         console.log(data);
-        
         return data
     }
 } 
