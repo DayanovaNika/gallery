b83ab73462d720a0947d9ed3b6fb9cfe84746fc1
Nika <Dayanova27@gmail.com>
2026-02-14 16:55:31 +0500

    feat: made base for header prewiew

---
+++ src/view/components/params/prewiew-params.ts
@@ -0,0 +1,9 @@
+import styleHeader from "../styles/prewiew.module.css"
+export const cardElementParams = {
+    tagName: "div",
+    classList: [],
+};
+export const prewiewheaderElParams = {
+    tagName: "div",
+    classList: [styleHeader.headerPrewiew],
+};
--- src/view/components/prewiew-view.ts
+++ src/view/components/prewiew-view.ts
@@ -1,16 +1,37 @@
 import Creator from "../../core/creator";
+import { cardElementParams, prewiewheaderElParams } from "./params/prewiew-params";
 
-const cardElementParams = {
-    tagName: "div",
-    classList: [],
-};
+class HeaderPrewiew {
+    prewiewHeaderElement
+
+    constructor({
+        // backdrop,
+    }){
+        console.log(prewiewheaderElParams);
+        // if(backdrop) {
+            
+        // }
+        this.prewiewHeaderElement = new Creator(prewiewheaderElParams).getElement()
+    }
+    getHeader(){
+        return this.prewiewHeaderElement
+    }
+}
 
 export class PrewiewView {
     cardElement
+    headerElement
+
     constructor(dataPrewiew){
         this.cardElement = new Creator(cardElementParams).getElement()
+        this.headerElement = new HeaderPrewiew({}).getHeader()
     }
+
     getPrewiew(){
         return this.cardElement
+    }
+
+    build(){
+        this.cardElement.append(this.headerElement)
     }
 }
