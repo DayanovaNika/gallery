e12d2426b8e8020a66dd6954a0ebbd21641b0f6e
Nika <Dayanova27@gmail.com>
2026-02-28 15:52:29 +0500

    feat: make header prewiew ctructure

--- src/view/components/params/prewiew-params.ts
+++ src/view/components/params/prewiew-params.ts
@@ -1,4 +1,5 @@
 import styleHeader from "../styles/prewiew.module.css";
+import stylePrewiew from '../styles/prewiew.module.css';
 
 export const prewiewheaderElParams = {
   tagName: "div",
@@ -8,8 +9,53 @@ export const prewiewheaderElParams = {
     style: "",
   },
 };
-
 export const cardElementParams = {
   tagName: "div",
   classList: [],
 };
+export const descFilmPrewiewParams = {
+    tagName: "div",
+    classList: [stylePrewiew.descFilmPrewiew],
+};
+export const genresTypeParams = {
+    tagName: "div",
+    classList: [stylePrewiew.genresType],
+    attributes: {
+    },
+    text: ""
+};
+export const genresParams = {
+    tagName: "span",
+    classList: [stylePrewiew.genres],
+    attributes: {
+    },
+    text: "",
+};
+export const filmNameParams = {
+    tagName: "h1",
+    text: "",
+    classList: [stylePrewiew.filmName],
+    attributes: {
+    },
+};
+export const infoWrapperParams = {
+    tagName: "div",
+    classList: [stylePrewiew.infoWrapper],
+    attributes: {
+    },
+};
+export const cellInfoParams = {
+    tagName: "div",
+    classList: [stylePrewiew.cellInfo],
+};
+export const titleInfoParams = {
+    tagName: "div",
+    text: "",
+    classList: [stylePrewiew.titleInfo],
+};
+export const valueInfoParams = {
+    tagName: "div",
+    classList: [stylePrewiew.valueInfo],
+    text: "",
+};
+
--- src/view/components/prewiew-view.ts
+++ src/view/components/prewiew-view.ts
@@ -1,46 +1,106 @@
 import Creator from "../../core/creator";
 import {
-  cardElementParams,
-  prewiewheaderElParams,
+    cardElementParams,
+    prewiewheaderElParams,
+    descFilmPrewiewParams,
+    genresTypeParams,
+    genresParams,
+    filmNameParams,
+    infoWrapperParams,
+    cellInfoParams,
+    titleInfoParams,
+    valueInfoParams,
 } from "./params/prewiew-params";
 
 class HeaderPrewiew {
-  prewiewHeaderElement;
-  constructor(dataPreview) {
+prewiewHeaderElement;
+constructor(dataPreview) {
     console.log(dataPreview);
 
-    // 3. вытащи из данных картинку и вставь вместо котика
-    const imgUrl =
-      "https://opis-cdn.tinkoffjournal.ru/mercury/black-cats-only-01.jpg";
-
-    // 4. тут мы просто дёргаем стайл атрибут из параметров и присваиваем ему строку с фоном, который мы хотим отобразить в превью
+    
+    const imgUrl = dataPreview.backdrop.url;
     prewiewheaderElParams.attributes.style = `background-image: url(${imgUrl});`;
 
-    this.prewiewHeaderElement = new Creator(prewiewheaderElParams).getElement();
-  }
+    const filmName = dataPreview.alternativeName
+    const ratingKp = dataPreview.rating.kp 
+    const ratingImdb = dataPreview.rating.imdb
+    const movieLength = dataPreview.movieLength
+    const ratingMpaa = dataPreview.ratingMpaa
+    const yearFilm = dataPreview.year
+    const filmType = dataPreview.type
+    const genres = dataPreview.genres
 
-  getHeader() {
+    const dataInfo = {
+        ["кинопоиск"]: ratingKp ?? "none",
+        ["IMDB"]: ratingImdb ?? "none",
+        ["год"]: yearFilm ?? "none",
+        ["продолжительность"]: movieLength ?? "none",
+        ["возрастной рейтинг"]: ratingMpaa ?? "none",
+    }
+    // const description = dataPreview.description
+    // const budget = dataPreview.budget
+    // const facts = dataPreview.facts
+    // const persons = dataPreview.persons
+    // const premiereWorld = dataPreview.premiere.world
+    // const premiereRussia = dataPreview.premiere.russia 
+    // const sequels = dataPreview.sequelsAndPrequels
+    // const similarMovies = dataPreview.similarMovies
+    
+    // 1 создать переменные в которые нужно вытащить данные
+
+    const descFilmPrewiew = new Creator(descFilmPrewiewParams).getElement();
+    genresTypeParams.text = filmType
+    const typeElement = new Creator(genresTypeParams).getElement()
+    this.prewiewHeaderElement = new Creator(prewiewheaderElParams).getElement();
+
+    descFilmPrewiew.append(typeElement)
+
+    genres.forEach(element => {
+        genresParams.text = element.name 
+        const genre = new Creator(genresParams).getElement()
+        descFilmPrewiew.append(genre)
+    });
+
+    this.prewiewHeaderElement.append(descFilmPrewiew)
+    filmNameParams.text = filmName
+    const filmNameElement = new Creator(filmNameParams).getElement()
+    this.prewiewHeaderElement.append(filmNameElement)
+
+    const infoWrapper = new Creator(infoWrapperParams).getElement()
+    for (const key in dataInfo) {
+        const cellInfo = new Creator(cellInfoParams).getElement()
+        titleInfoParams.text = key
+
+        const titleInfo = new Creator(titleInfoParams).getElement()
+        valueInfoParams.text = dataInfo[key]
+
+        const valueInfo = new Creator(valueInfoParams).getElement()
+        cellInfo.append(titleInfo,valueInfo)
+        infoWrapper.append(cellInfo)
+    }
+    this.prewiewHeaderElement.append(infoWrapper)
+}
+
+getHeader() {
     return this.prewiewHeaderElement;
-  }
+}
 }
 
 export class PreviewView {
-  cardElement;
-  headerElement;
+    cardElement;
+    headerElement;
 
-  // 1. dataPreview - данные для отображения в превью, например, url картинки (и много чего еще)
-  constructor(dataPreview) {
+    constructor(dataPreview) {
     this.cardElement = new Creator(cardElementParams).getElement();
-    // 2. передаем данные для отображения в превью в HeaderPrewiew, чтобы он мог отобразить эти данные
     this.headerElement = new HeaderPrewiew(dataPreview).getHeader();
     this.build();
-  }
+}
 
-  getPrewiew() {
+getPrewiew() {
     return this.cardElement;
-  }
+}
 
-  build() {
+build() {
     this.cardElement.append(this.headerElement);
-  }
+}
 }
